a387ba43c01a65f31c7263bb06edff1212b4efea
steven <steven@stevenw.org>
2018-04-06 22:18:16 -0600

    Init commit

---
+++ app/OnlineSession.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OnlineSession extends Model
+{
+    function getLastSession() {}
+    function isCurrentlyOnline() {
+
+    }
+    function getSessions() {
+
+    }
+    function getLoginAmounts() {
+
+    }
+    function getPlayTime() {
+
+    }
+}
